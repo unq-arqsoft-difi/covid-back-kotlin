257dd47eb2a3534279c32868a1c624981c8106c2
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-05-10 20:12:35 -0300

    :green_heart: adapt tests to database and closes #7

--- src/test/kotlin/difi/covid/api/APITestCase.kt
+++ src/test/kotlin/difi/covid/api/APITestCase.kt
@@ -2,7 +2,13 @@ package difi.covid.api
 
 import com.github.kittinunf.fuel.core.FuelManager
 import difi.covid.CovidApp
+import difi.covid.database.UserTable
 import io.javalin.Javalin
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.*
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -12,6 +18,11 @@ abstract class APITestCase {
 
     @BeforeAll
     fun setUp() {
+        Database.connect("jdbc:sqlite:file:test", "org.sqlite.JDBC")
+        transaction {
+            addLogger(StdOutSqlLogger)
+            SchemaUtils.create(UserTable)
+        }
         api = CovidAPI(8000).startWith(CovidApp())
         // Inject the base path to no have repeat the whole URL
         FuelManager.instance.basePath = "http://localhost:${api.port()}/"
@@ -20,5 +31,9 @@ abstract class APITestCase {
     @AfterAll
     fun tearDown() {
         api.stop()
+        transaction {
+            addLogger(StdOutSqlLogger)
+            SchemaUtils.drop(UserTable)
+        }
     }
 }
